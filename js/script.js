c7a27a5ca3c9d52c9f26f7d2e502a9f0be657cbb
bcipolla-uade <bcipolla@uade.edu.ar>
2025-06-15 21:09:29 -0300

    chatbot

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function mostrarRespuesta(opcion) {
+  if (opcion === 'info') {
+    alert('Somos una feria sustentable que promueve productos ecológicos.');
+  } else if (opcion === 'contacto') {
+    alert('Podés escribirnos a info@raicessustentables.com');
+  } else if (opcion === 'redes') {
+    alert('Seguinos en Instagram: @raicessustentables ');
+  }
+}
