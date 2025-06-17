15cbcf7c0a7f3462d67f46931a507934351b44c4
Jonathan Tola <jonathan.tola@pm.me>
2025-06-16 21:57:43 -0300

    se agrega chatbot

--- src/js/script.js
+++ src/js/script.js
@@ -41,3 +41,13 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 });
+
+const mostrarRespuesta = (opcion) => {
+    if (opcion === 'info') {
+        alert('Somos una feria sustentable que promueve productos ecológicos.');
+    } else if (opcion === 'contacto') {
+        alert('Podés escribirnos a info@raicessustentables.com');
+    } else if (opcion === 'redes') {
+        alert('Seguinos en Instagram: @raicessustentables ');
+    }
+}
